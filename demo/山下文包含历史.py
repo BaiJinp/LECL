6b32d65ce616b226b5fa3e4ef34a188828c60505
jinpeng.bai <jinpeng.bai@transsion.com>
2025-04-03 18:05:02 +0800

    first

--- demo/山下文包含历史.py
+++ demo/山下文包含历史.py
@@ -1,9 +1,11 @@
+import sqlite3
+
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables import RunnableWithMessageHistory
 from langchain_openai import ChatOpenAI
-from langchain.memory import SQLChatMessageHistory
-
+# from langchain.memory import SQLChatMessageHistory
+from langchain_community.chat_message_histories import SQLChatMessageHistory
 # 初始化 LLM
 llm = ChatOpenAI(temperature=0.9,
                  model="deepseek-chat",
@@ -12,7 +14,7 @@ llm = ChatOpenAI(temperature=0.9,
 
 # 定义提示词模板
 prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are a helpful assistant that translates {input_language} to {output_language}."),
+    ("system", "你现在是一个地理专家"),
     MessagesPlaceholder(variable_name="history"),  # 占位符用于历史消息
     ("user", "{text}"),
     ("assistant", "{text}"),
@@ -27,12 +29,39 @@ chain = prompt | llm | parser
 # 获取会话函数
 def getSession(sid):
     # 使用 SQLite 数据库存储聊天历史
-    return SQLChatMessageHistory(session_id=sid, connection="sqlite:///history.db")
+    return SQLChatMessageHistory(sid, "sqlite:///history.db")
+
+# 创建 message_history 表
+def create_message_history_table(db_path):
+    create_table_sql = """
+    CREATE TABLE IF NOT EXISTS message_history (
+        session_id TEXT,
+        message_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        role TEXT,
+        content TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+    """
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute(create_table_sql)
+    conn.commit()
+    conn.close()
+
+# 数据库路径
+db_path = "history.db"
+
+# 创建表
+create_message_history_table(db_path)
 
 # 将会话历史集成到链中
 runnable = RunnableWithMessageHistory(chain, getSession, input_messages_key='text', history_messages_key="history")
 # 调用链并传入参数
-resp = runnable.invoke({"input_language": "Chinese", "output_language": "Japanese", "text": "小日本"},
+resp = runnable.invoke({"text": "日本"},
+                       config={"configurable": {"session_id": "123"}})
+
+resp1 = runnable.invoke({"text": "这个国家有哪些著名的城市"},
                        config={"configurable": {"session_id": "123"}})
 # 打印结果
 print(resp)
+print(resp1)
